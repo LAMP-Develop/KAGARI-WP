5932c4fd08fc91fb9e35fa74b6867fc87154ee54
ShunyaSuzuki <anyushu2017@gmail.com>
2019-12-20 18:51:16 +0900

    update

--- lib/func/add-func.php
+++ lib/func/add-func.php
@@ -25,3 +25,15 @@ function customize_manage_posts_custom_column($column_name, $post_id)
     }
 }
 add_action('manage_posts_custom_column', 'customize_manage_posts_custom_column', 10, 2);
+
+// RSSにアイキャッチ含める
+function rss_post_thumbnail($content)
+{
+    global $post;
+    if (has_post_thumbnail($post->ID)) {
+        $content = '<p>'.get_the_post_thumbnail($post->ID).'</p>'.$content;
+    }
+    return $content;
+}
+add_filter('the_excerpt_rss', 'rss_post_thumbnail');
+add_filter('the_content_feed', 'rss_post_thumbnail');
